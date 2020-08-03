b0680577a324ce9bf52e233b0cd847e10afdfeb5
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-08-03 22:15:54 +0900

    javascript-basic / project / light box update

---
+++ chap07/js/app.js
@@ -0,0 +1,29 @@
+const indicator = document.querySelectorAll('.indicator button');
+const lightbox = document.querySelector('#lightbox');
+
+//라이트 박스 배경
+const block = document.querySelector('#block'); 
+
+//라이트 박스 표시
+function lightbox_open(num) {
+    lightbox.setAttribute('class', 'active');
+    block.setAttribute('class', 'active');
+    change_img(num);
+    indicator[num-1].focus();
+}
+
+//라이트 박스 닫기
+function lightbox_close() {
+    lightbox.removeAttribute('class');
+    block.removeAttribute('class');
+}
+
+//이;미지 전환 표시 함수
+function change_img(val) {
+    let imgs = document.querySelectorAll('figure > img');
+
+    for(let i = 0; i < imgs.length; i++) {
+        imgs[i].removeAttribute('class');
+    }
+    imgs[val-1].setAttribute('class', 'active');
+}
